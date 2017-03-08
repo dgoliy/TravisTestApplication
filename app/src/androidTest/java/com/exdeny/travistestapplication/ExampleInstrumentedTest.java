d9cc8d3f5058bc0c3ac74890debdad1b7d7c1c51
Dmytro Goliy <dmytro@gfycat.com>
2017-03-07 17:27:09 -0800

    Fix instrumentedTest

--- app/src/androidTest/java/com/exdeny/travistestapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/exdeny/travistestapplication/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.exdeny.travistestapplication.fake", appContext.getPackageName());
+        assertEquals("com.exdeny.travistestapplication", appContext.getPackageName());
     }
 }
